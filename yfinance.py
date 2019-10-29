9fb2a360a42ff7f402c9b9fa0eb7e4695bc799fb
evanafonseka <49772033+evanafonseka@users.noreply.github.com>
2019-10-29 17:48:49 +1100

    Update yfinance.py

--- yfinance.py
+++ yfinance.py
@@ -1,8 +1,45 @@
-#yfinance wrapper
-
 import yfinance as yf
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
 
 banks = ("NAB.AX","WBC.AX","CBA.AX","ANZ.AX","MQG.AX")
-data = yf.download(banks, period="60d",interval="5m")
+data = yf.download(banks, period="max",interval="1d")
 df = data.iloc[:,0:5]
+
+#Calculate historical VaR
+#Calculate Standard Deviation VaR
+
+log_ret = np.log(df/df.shift(1))
+
+np.random.seed(42)
+num_ports = 10000
+all_weights = np.zeros((num_ports, len(df.columns)))
+ret_arr = np.zeros(num_ports)
+vol_arr = np.zeros(num_ports)
+sharpe_arr = np.zeros(num_ports)
+
+for x in range(num_ports):
+    # Weights
+    weights = np.array(np.random.random(5))
+    weights = weights/np.sum(weights)
+    all_weights[x,:] = weights
+    
+    # Expected return
+    ret_arr[x] = np.sum( (log_ret.mean() * weights * 252))
+    
+    # Expected volatility
+    vol_arr[x] = np.sqrt(np.dot(weights.T, np.dot(log_ret.cov()*252, weights)))
+    
+    # Sharpe Ratio
+    sharpe_arr[x] = ret_arr[x]/vol_arr[x]
+
+
+plt.figure(figsize=(12,8))
+plt.scatter(vol_arr, ret_arr, c=sharpe_arr, cmap='viridis')
+plt.colorbar(label='Sharpe Ratio')
+plt.xlabel('Volatility')
+plt.ylabel('Return')
+plt.show()
+
 
